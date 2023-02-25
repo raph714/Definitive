8991e22b7577eb711b9446e0c624418176f44d87
Raphael DeFranco <raphael.defranco@gmail.com>
2023-02-25 12:49:36 -0800

    Initial Commit

---
+++ Definitive/DefinitiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefinitiveApp.swift
+//  Definitive
+//
+//  Created by Raphael DeFranco on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DefinitiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
